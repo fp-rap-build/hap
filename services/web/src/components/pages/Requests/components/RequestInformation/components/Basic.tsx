8067382c231d27df2a7e7f010100c6077a59ce85
iesous-kurios <j.wylie.81@gmail.com>
2021-05-04 00:41:30 -0700

    adding advocate question, did correct migration for new data element

--- services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
+++ services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
@@ -19,9 +19,7 @@ export default function Basic({ request, column = 2 }) {
       <Descriptions.Item label="Unemployed for 90 days">
         {request.unEmp90 ? 'Yes' : 'No'}
       </Descriptions.Item>
-      <Descriptions.Item label="Advocate?">
-        {request.advocate ? 'Yes' : 'No'}
-      </Descriptions.Item>
+      
     </Descriptions>
   );
 }
