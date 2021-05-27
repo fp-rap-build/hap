e894880df61e461f43353f40033181b2f66776eb
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-05-27 10:41:14 -0400

    Changed user to payload inside userModel

--- services/server/api/routes/users/userModel.js
+++ services/server/api/routes/users/userModel.js
@@ -32,7 +32,7 @@ const findById = async (id) => db('users').where({ id }).first('*');
 
 const findByIdAndUpdate = async (id, payload) => {
   if (payload['password']) {
-    payload['password'] = await bcrypt.hash(user['password'], salt);
+    payload['password'] = await bcrypt.hash(payload['password'], salt);
   }
   return await db('users').where({ id }).update(payload).returning('*');
 };
