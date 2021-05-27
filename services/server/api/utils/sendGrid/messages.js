31ce21a09b89f138adf757315207cabbd4e73088
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-05-27 12:40:54 -0400

    Sending different emails for certain status changes

--- services/server/api/utils/sendGrid/messages.js
+++ services/server/api/utils/sendGrid/messages.js
@@ -2,13 +2,48 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const requestStatusChange = (requestStatus, emailAddress) => {
+  let text;
+
+  switch (requestStatus) {
+    case 'inReview':
+      text =
+        'Your Family Promise HAP Application status is being reviewed by our team!';
+      break;
+
+    case 'documentsNeeded':
+      text = `Your Family Promise HAP Application requires documents to continue forward. Please login to your account to view which documents to upload ${process.env.REACT_APP_URI}/login`;
+      break;
+
+    case 'verifyingDocuments':
+      text = `Our team is currently verifying your documents! You can login to view any new comments here ${process.env.REACT_APP_URI}/login`;
+      break;
+
+    case 'notResponding':
+      text =
+        'Our team has decided to put your HAP Application on hold until you respond';
+      break;
+
+    case 'denied':
+      text = 'Your Family Promise HAP Application has been denied';
+      break;
+
+    case 'approved':
+      text = 'Your Family Promise HAP Application has been approved!';
+      break;
+
+    default:
+      text = `Your Family Promise HAP Application status has been changed to ${requestStatus}`;
+      break;
+  }
+
   const msg = {
     to: emailAddress,
     from: 'admin@familypromiseofspokane.org',
     subject: 'Update! Your request status has changed',
-    text: `Your Family Promise HAP Application status has been updated to ${requestStatus}`,
-    html: `<p>Your Family Promise HAP Application status has been updated to <strong>${requestStatus}!</strong></p>`,
+    text: text,
+    html: `<p>${text}</p>`,
   };
+
   sgMail
     .send(msg)
     .then(() => {
