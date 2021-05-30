a11c8fafa8f4303029faff3422509869a9a16810
joeywire <61367542+joeywire@users.noreply.github.com>
2021-05-30 14:07:24 -0500

    add status and category columns to docs

--- services/web/src/components/pages/Home/components/DefaultHomePage/Documents/index.jsx
+++ services/web/src/components/pages/Home/components/DefaultHomePage/Documents/index.jsx
@@ -25,6 +25,14 @@ const docDescriptions = [
   // { category: 'other', title: 'Additional Documents', blurb: '' },
 ];
 
+//If they opt out pop out a modal that reads the form with a I Acknowledge etc. etc
+// toggle status to optOut
+//If docs good toggles status to received
+//------------------------------------------------
+//For status/ category trackingn handle on FE
+//Write a list of the four categories - get all docs for the request
+// show x/4 received, x/4 optOut, x/4 missing
+
 export default function Index({ request }) {
   return (
     <div className="documentsContainer">
