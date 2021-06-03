8cbdb93345f90cbeabc6d8329f166eee5467b968
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-03 16:15:48 -0500

    re-orderd user dash menu

--- services/web/src/components/pages/Home/components/DefaultHomePage/UserInfo/RenderMenu.tsx
+++ services/web/src/components/pages/Home/components/DefaultHomePage/UserInfo/RenderMenu.tsx
@@ -11,7 +11,7 @@ const RenderMenu = ({ onContentChange }) => {
 
   const menu = (
     <Menu
-      theme="dark"
+      theme="light"
       defaultSelectedKeys={['address']}
       mode="inline"
       className="userSidebar"
