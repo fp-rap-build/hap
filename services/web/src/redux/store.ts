d5a09ee885166fb9bb7f295ea45c3c6a5aab205e
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-04 21:00:29 -0500

    refresh bug fixed

--- services/web/src/redux/store.ts
+++ services/web/src/redux/store.ts
@@ -1,10 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
+
 import rootReducer from './rootReducer';
 
 const middlewares = [logger, thunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//Setting up redux-persist
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+  //only persisting requests, can add any top level state here i.e users, notifications
+  whitelist: ['requests'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer, applyMiddleware(...middlewares));
+let persistor = persistStore(store);
+//eslint-disable-next-line
+export { store, persistor };
