7cbc88a6480462a813713dd6fcfc5ea087924fc3
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-08 11:45:36 -0500

    fixed tiny star on mobile view of apply forms

--- services/web/src/components/pages/Apply/FPTitle.tsx
+++ services/web/src/components/pages/Apply/FPTitle.tsx
@@ -1,14 +1,20 @@
-import { Typography, Image } from 'antd';
+import useWindowDimensions from '../../../utils/useWindowDimensions';
 
 import goldStar from '../../../assets/FP-star-gold.png';
 
+import { Typography, Image } from 'antd';
 const { Title } = Typography;
 
 const FPTitle = ({ title }) => {
+  const { width } = useWindowDimensions();
+
   return (
     <div className="fpCardTitle">
-      <Image width={'1rem'} src={goldStar} />
-      <Title level={3} style={{ color: 'white', marginLeft: '30px' }}>
+      <Image width={'2rem'} src={goldStar} />
+      <Title
+        level={width < 400 ? 5 : 4}
+        style={{ color: 'white', marginLeft: '5%' }}
+      >
         {title}
       </Title>
     </div>
