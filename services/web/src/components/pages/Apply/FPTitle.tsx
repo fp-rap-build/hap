208717395a397b3bb024968c77e0d427c978f96d
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-08 11:27:18 -0500

    removed num of  children field from initial application

--- services/web/src/components/pages/Apply/FPTitle.tsx
+++ services/web/src/components/pages/Apply/FPTitle.tsx
@@ -1,14 +1,13 @@
 import { Typography, Image } from 'antd';
 
 import goldStar from '../../../assets/FP-star-gold.png';
-import logo from '../../../assets/logo2.png';
 
 const { Title } = Typography;
 
 const FPTitle = ({ title }) => {
   return (
     <div className="fpCardTitle">
-      <Image width={50} src={goldStar} />
+      <Image width={'1rem'} src={goldStar} />
       <Title level={3} style={{ color: 'white', marginLeft: '30px' }}>
         {title}
       </Title>
