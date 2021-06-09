ad7422a7d12a903c46fea8e91d93ab19c5cc212e
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-06-09 19:33:10 -0400

    Joined with programs table to get the name of the program

--- services/server/api/routes/payments/model.js
+++ services/server/api/routes/payments/model.js
@@ -4,7 +4,15 @@ exports.findForTable = () =>
   db('payments as p')
     .join('requests as r', 'p.requestId', '=', 'r.id')
     .join('users as u', 'r.userId', '=', 'u.id')
-    .select('p.id', 'u.firstName', 'u.lastName', 'u.email', 'p.amount');
+    .join('programs as pr', 'p.programId', '=', 'pr.id')
+    .select(
+      'p.id',
+      'u.firstName',
+      'u.lastName',
+      'u.email',
+      'pr.name as program',
+      'p.amount'
+    );
 
 exports.findById = (id) => db('payments').where({ id }).first();
 
