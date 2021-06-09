e33207b475cc5a6e1e22be6d3a96b14acea45404
Isaiah Fowler <isaiah@Isaiahs-MacBook-Air.local>
2021-06-09 19:34:52 -0400

    added program to payments table

--- services/web/src/components/pages/Admin/components/PaymentsTable.js
+++ services/web/src/components/pages/Admin/components/PaymentsTable.js
@@ -16,6 +16,13 @@ export default function PaymentsTable() {
     { title: 'Last ', field: 'lastName', editable: 'never' },
     { title: 'Email', field: 'email', type: 'string', editable: 'never' },
     {
+      title: 'Program',
+      field: 'program',
+      type: 'string',
+      editable: 'never',
+    },
+
+    {
       title: 'Amount',
       field: 'amount',
       type: 'integer',
