b8a2a54a60203b3063acff70d37da220cce23845
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-16 23:31:22 -0500

    styling tweak

--- services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
+++ services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
@@ -7,7 +7,7 @@ export default function Basic({ ages, request }) {
     .join(', ');
 
   return (
-    <Descriptions column={2}>
+    <Descriptions column={2} style={{ paddingBottom: '5%' }}>
       <Descriptions.Item label="Name">{`${request.firstName} ${request.lastName}`}</Descriptions.Item>
       <Descriptions.Item label="State">{request.state}</Descriptions.Item>
       <Descriptions.Item label="Email">
