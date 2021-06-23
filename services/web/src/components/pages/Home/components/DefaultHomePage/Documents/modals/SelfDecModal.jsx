a4152ece9bf44de3d992d21fba48ab7319686227
joeywire <61367542+joeywire@users.noreply.github.com>
2021-06-22 22:30:34 -0500

    comments posting w/ correct date and time

--- services/web/src/components/pages/Home/components/DefaultHomePage/Documents/modals/SelfDecModal.jsx
+++ services/web/src/components/pages/Home/components/DefaultHomePage/Documents/modals/SelfDecModal.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { axiosWithAuth } from '../../../../../../../api/axiosWithAuth';
 
-import { Modal, Typography, Button, Checkbox } from 'antd';
 import { fetchDocuments } from '../../../../../../../redux/requests/requestActions';
+
+import { Modal, Typography, Button, Checkbox, Form, Input } from 'antd';
 const { Paragraph, Title } = Typography;
+const { TextArea } = Input;
 
 const SelfDecModal = ({
   selfDecModalVisibility,
@@ -17,10 +19,12 @@ const SelfDecModal = ({
   updateLocalStatuses,
   tableData,
 }) => {
+  const currentUser = useSelector(state => state.user.currentUser);
+
   const [checked, setChecked] = useState(false);
+  const [userText, setUserText] = useState('');
 
   const dispatch = useDispatch();
-
   const fetchUserDocuments = () => dispatch(fetchDocuments(request.id));
 
   //Adding place holder doc now as confirmation the user completed this process.
@@ -48,6 +52,30 @@ const SelfDecModal = ({
     }
   };
 
+  const postToComments = async userText => {
+    const reqBody = {
+      requestId: request.id,
+      authorId: currentUser.id,
+      comment: userText,
+      category: 'external',
+      createdAt: new Date().toISOString(),
+    };
+
+    try {
+      await axiosWithAuth().post(`comments/`, reqBody);
+    } catch (error) {
+      alert('ERROR POSTING COMMENT');
+      console.log(error);
+    }
+  };
+
+  const onFinish = value => {
+    setUserText(value.text);
+    postToComments(
+      `${selectedCategory} self declaration explanation: ${value.text}`
+    );
+  };
+
   return (
     <>
       <Modal
@@ -55,6 +83,7 @@ const SelfDecModal = ({
         visible={selfDecModalVisibility}
         bodyStyle={{ height: '20vh' }}
         onCancel={handleCancel}
+        maskClosable={false}
         footer={[
           <>
             <Button
@@ -80,7 +109,33 @@ const SelfDecModal = ({
           </>,
         ]}
       >
-        <Paragraph>
+        <div className="selfDecContent">
+          <Form layout="vertical" name="selfDecUserInput" onFinish={onFinish}>
+            <Form.Item
+              name="text"
+              label="Briefly explain why you cannot provide the requested document:"
+              initialValue={userText}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please explain why you cannot provide a document!',
+                },
+                {
+                  min: 20,
+                  message: 'Explanation must be at least 20 characters!',
+                },
+              ]}
+            >
+              <TextArea key="fix" rows={5} allowClear />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        {/* <Paragraph>
           By clicking below I am stating that I am unable to provide
           documentation for category: {selectedCategory}. And that I am prepared
           to provide a detailed explanation of my current status in lieu of
@@ -88,7 +143,7 @@ const SelfDecModal = ({
         </Paragraph>
         <Checkbox checked={checked} onChange={() => setChecked(!checked)}>
           I have read and understand the statement above
-        </Checkbox>
+        </Checkbox> */}
       </Modal>
     </>
   );
