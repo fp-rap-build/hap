fd60ec3561586466f08865ddf1a03d41ae6d62af
Isaiah Fowler <greatpup1940@gmail.com>
2021-06-24 09:53:41 -0400

    Moved POST request logic to a separate function

---
+++ services/server/api/routes/requests/controllers.js
@@ -0,0 +1,38 @@
+const Requests = require('../requests/requestsRouter');
+
+const Addresses = require('../addresses/addr-model');
+
+exports.createRequest = async (req, res, next) => {
+  try {
+    const request = req.body;
+
+    // Create a new address
+    const addressInfo = request['address'] || {};
+
+    const address = await Addresses.create(addressInfo);
+
+    // Default to the current users ID if one isn't specified
+    request['userId'] = request['userId'] || req.user.id;
+
+    // Link the new address to the request
+    request['addressId'] = address[0].id;
+
+    // Remove the address before saving to db
+    delete request['address'];
+
+    // Create entry in age for head of household
+    await Ages.create({ userId: req.user.id, role: 'headOfHousehold' });
+
+    //Create age entries for children
+    for (let i = 0; i < request.totalChildren; i++) {
+      await Ages.create({ userId: req.user.id, role: 'child' });
+    }
+
+    const newRequest = await Requests.create(request);
+
+    res.status(200).json(newRequest[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
