a9bad6cc03b2cc80119cda56d15f908bb7ace9b3
joeywire <61367542+joeywire@users.noreply.github.com>
2021-07-05 21:28:34 -0500

    middleware built

--- services/server/api/routes/documents/validators/index.js
+++ services/server/api/routes/documents/validators/index.js
@@ -1,4 +1,5 @@
 const Documents = require('../documentModel');
+const Requests = require('../../requests/requestsModel');
 
 const checkIfDocumentExists = async (req, res, next) => {
   const { id } = req.params;
@@ -18,4 +19,61 @@ const checkIfDocumentExists = async (req, res, next) => {
   }
 };
 
-module.exports = { checkIfDocumentExists };
+const checkDocumentCompletion = async (req, res, next) => {
+  /* 
+  Take in the new document (req.body)
+  Fetch all the documents for request - check to see if each category is uploaded 
+  If category is other continue 
+  */
+
+  const { requestId } = req.body;
+
+  const docChecks = {
+    childrenOrPregnancy: false,
+    residency: false,
+    income: false,
+    housingInstability: false,
+    covid: false,
+  };
+
+  //Update status of current document being processed
+  docChecks[req.body.category] = true;
+
+  //Check agains other docs assigned to req - skipping any 'other' docs
+  try {
+    const request = await Requests.findById(requestId);
+
+    if (request.requestStatus !== 'documentsNeeded') {
+      console.log(`Request status: ${request.status} -- next`);
+      next();
+    }
+
+    const requestDocs = await Documents.findByRequestId(requestId);
+
+    requestDocs.forEach((doc) => {
+      if (
+        (doc.status === 'received' || doc.status === 'optOut') &&
+        doc.category !== 'other'
+      ) {
+        docChecks[doc.category] = true;
+      }
+    });
+
+    console.log(docChecks);
+
+    //Check to see if docChecks has any false values
+    var hasFalseDocs = Object.keys(docChecks).some((key) => !docChecks[key]);
+
+    //No false's --> update doc status
+    if (!hasFalseDocs) {
+      Request.update(requestId, { status: 'verifyingDocuments' });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+module.exports = { checkIfDocumentExists, checkDocumentCompletion };
--- services/server/data/migrations/20210530042642_adding_status_to_documents.js
+++ services/server/data/migrations/20210530042642_adding_status_to_documents.js
@@ -1,13 +1,4 @@
-//Need to add Category Enum (the five we defined on the fron end)
-//Need to add Status Enum (received, optOut, verified, denied)
-//Write or find enpoint to edit document
-//If they opt out pop out a modal that reads the form with a I Acknowledge etc. etc
-// toggle status to optOut
-//If docs good toggles status to received
-//------------------------------------------------
-//For status/ category trackingn handle on FE
-//Write a list of the four categories - get all docs for the request
-// show x/4 received, x/4 optOut, x/4 missing
+
 
 exports.up = function (knex) {
   return knex.schema.table('documents', (tbl) => {
