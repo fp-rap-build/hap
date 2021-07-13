e3e53e412136a99257ec45929ced22e1d73e6300
joeywire <61367542+joeywire@users.noreply.github.com>
2021-07-13 14:38:51 -0500

    fixed format date function

--- services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
+++ services/web/src/components/pages/Requests/components/RequestInformation/components/Basic.tsx
@@ -8,7 +8,7 @@ export default function Basic({ request, setRequest, column = 2 }) {
   const [disabled, setDisabled] = useState(true);
 
   const formatDate = date => {
-    // if (!date) return 'YYYY / MM / DD';
+    if (!date) return;
 
     const splitDate = date.split('T');
     return splitDate[0];
@@ -84,67 +84,4 @@ export default function Basic({ request, setRequest, column = 2 }) {
       />
     </Form>
   );
-}
-
-{
-  /* <Form.Item
-        label="First Name"
-        name="firstName"
-        initialValue={request.firstName}
-      >
-        <Input disabled={disabled} />
-      </Form.Item>
-
-      <Form.Item
-        label="Last Name"
-        name="lastName"
-        initialValue={request.lastName}
-      >
-        <Input disabled={disabled} defaultValue={request.lastName} />
-      </Form.Item>
-
-      <Form.Item label="State" name="state" initialValue={request.state}>
-        <Input disabled={disabled} defaultValue={request.state} />
-      </Form.Item>
-      <Form.Item label="Email" name="email" initialValue={request.email}>
-        <Input disabled={disabled} defaultValue={request.email} />
-      </Form.Item>
-
-      <Form.Item label="City" name="cityName" initialValue={request.cityName}>
-        <Input disabled={disabled} defaultValue={request.cityName} />
-      </Form.Item>
-      <Form.Item
-        label="Phone Number"
-        name="tenantNumber"
-        initialValue={request.tenantNumber}
-      >
-        <Input disabled={disabled} defaultValue={request.tenantNumber} />
-      </Form.Item>
-
-      <Form.Item label="Role" name="role" initialValue={request.role}>
-        <Input disabled={disabled} defaultValue={request.role} />
-      </Form.Item>
-      <Form.Item label="Zip" name="zipCode" initialValue={request.zipCode}>
-        <Input disabled={disabled} defaultValue={request.zipCode} />
-      </Form.Item>
-
-      <Form.Item label="Address" name="address" initialValue={request.address}>
-        <Input disabled={disabled} defaultValue={request.address} />
-      </Form.Item>
-
-      <Form.Item
-        label="Food Worker"
-        name="foodWrkr"
-        initialValue={request.foodWrkr}
-      >
-        <Input disabled={disabled} defaultValue={request.foodWrkr} />
-      </Form.Item>
-      <Form.Item
-        label="Unemployed for 90+ days"
-        name="unEmp90"
-        initialValue={request.unEmp90}
-      >
-        <Input disabled={disabled} defaultValue={request.unEmp90} />
-      </Form.Item>
-      <Button htmlType="submit">Submit</Button> */
 }
