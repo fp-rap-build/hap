026042159b38efa8f4fee8fc4afc5af6a038a7de
Isaiah Fowler <greatpup1940@gmail.com>
2021-07-14 05:49:04 -0400

    Fixed manually setting inital form values

--- services/web/src/components/pages/Apply/index.tsx
+++ services/web/src/components/pages/Apply/index.tsx
@@ -172,7 +172,11 @@ export default function Index() {
 
   const goBackwards = () => setStep(step - 1);
 
-  const [formValues, setFormValues] = useState(INITIAL_VALUES_DEV);
+  const [formValues, setFormValues] = useState(
+    process.env.NODE_ENV === 'development'
+      ? INITIAL_VALUES_DEV
+      : INITIAL_VALUES_PROD
+  );
   const [formConsent, setFormConsent] = useState(false);
 
   const handleChange = e => {
