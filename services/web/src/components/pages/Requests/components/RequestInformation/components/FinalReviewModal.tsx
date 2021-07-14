84c8419d7f2e898c073dee57bd125e4b28625574
Isaiah Fowler <greatpup1940@gmail.com>
2021-07-13 20:40:36 -0400

    Validating fields

--- services/web/src/components/pages/Requests/components/RequestInformation/components/FinalReviewModal.tsx
+++ services/web/src/components/pages/Requests/components/RequestInformation/components/FinalReviewModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Modal, Input, message, Form, Select } from 'antd';
 
@@ -17,23 +17,91 @@ export default function FinalReviewModal({
 }) {
   const [form] = Form.useForm();
 
+  const [loading, setLoading] = useState(false);
+
   const onOk = () => {
+    let values = form.getFieldsValue();
+
+    for (let field in values) {
+      if (!values[field]) {
+        form.scrollToField(field);
+
+        return message.error('Please fill out all fields');
+      }
+    }
+
     form.submit();
   };
 
-  const handleFinalReview = values => {
-    console.log(values);
+  const handleFinalReview = async values => {
+    const requestValues = {
+      budget: values.budget,
+      landlordName: values.landlordName,
+      landlordAddress: values.landlordAddress,
+      landlordAddress2: values.landlordAddress2,
+      landlordCity: values.landlordCity,
+      landlordState: values.landlordState,
+      landlordZip: values.landlordZip,
+      landlordEmail: values.landlordEmail,
+      amountApproved: values.amountApproved,
+    };
+
+    const userValues = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+    };
+
+    const addressValues = {
+      address: values.address,
+      cityName: values.cityName,
+      state: values.state,
+      zipCode: values.zipCode,
+    };
+
+    setLoading(true);
+    try {
+      await axiosWithAuth().put(`/requests/${request.id}`, requestValues);
+      await axiosWithAuth().put(
+        `/requests/${request.id}/address`,
+        addressValues
+      );
+
+      await axiosWithAuth().put(`/users/${request.userId}`, userValues);
+
+      setRequest({ ...request, ...values });
+
+      setVisible(false);
+      showApprovedModal();
+    } catch (error) {
+      message.error('Unable to update request');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Modal
       title={'Final Review'}
       visible={visible}
-      onCancel={() => setVisible(false)}
+      onCancel={() => {
+        form.resetFields();
+
+        setVisible(false);
+      }}
       onOk={onOk}
+      confirmLoading={loading}
     >
       <Form onFinish={handleFinalReview} layout="vertical" form={form}>
-        <Form.Item name="budget" label="Budget">
+        <Form.Item
+          initialValue={request.budget}
+          name="budget"
+          label="Budget"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Select defaultValue={request.budget}>
             {programs.map(program => (
               <Option value={program.name}>{program.name}</Option>
@@ -44,57 +112,180 @@ export default function FinalReviewModal({
           name="landlordName"
           label="Landlord Name"
           initialValue={request.landlordName}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
         >
           <Input
             defaultValue={request.landlordName}
             value={request.landlordName}
+            required={true}
           />
         </Form.Item>
-        <Form.Item label="Landlord Address">
+        <Form.Item
+          name="landlordAddress"
+          initialValue={request.landlordAddress}
+          label="Landlord Address"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.landlordAddress} />
         </Form.Item>
-        <Form.Item label="Landlord Address Line 2 (optional)">
+        <Form.Item
+          name="landlordAddress2"
+          initialValue={request.landlordAddress2}
+          label="Landlord Address Line 2"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.landlordAddress2} />
         </Form.Item>
-        <Form.Item label="Landlord City">
+        <Form.Item
+          name="landlordCity"
+          initialValue={request.landlordCity}
+          label="Landlord City"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.landlordCity} />
         </Form.Item>
-        <Form.Item label="Landlord state">
+        <Form.Item
+          name="landlordState"
+          initialValue={request.landlordState}
+          label="Landlord state"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Select defaultValue={request.landlordState}>
             {states.map(state => (
               <Option value={state}>{state}</Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item label="Landlord Zip">
+        <Form.Item
+          name="landlordZip"
+          initialValue={request.landlordZip}
+          label="Landlord Zip"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.landlordZip} />
         </Form.Item>
-        <Form.Item label="Landlord Email">
+        <Form.Item
+          name="landlordEmail"
+          initialValue={request.landlordEmail}
+          label="Landlord Email"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.landlordEmail} />
         </Form.Item>
-        <Form.Item label="Amount Approved">
+        <Form.Item
+          name="amountApproved"
+          initialValue={request.amountApproved}
+          label="Amount Approved"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.amountApproved} />
         </Form.Item>
-        <Form.Item label="First Name">
+        <Form.Item
+          name="firstName"
+          initialValue={request.firstName}
+          label="First Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.firstName} />
         </Form.Item>
-        <Form.Item label="Last Name">
+        <Form.Item
+          name="lastName"
+          initialValue={request.lastName}
+          label="Last Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.lastName} />
         </Form.Item>
-        <Form.Item label="Address">
+        <Form.Item
+          name="address"
+          initialValue={request.address}
+          label="Address"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.address} />
         </Form.Item>
-        <Form.Item label="City">
+        <Form.Item
+          name="cityName"
+          initialValue={request.cityName}
+          label="City"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.cityName} />
         </Form.Item>
-        <Form.Item label="State">
-          <Select defaultValue={request.landlordState}>
+        <Form.Item
+          name="state"
+          initialValue={request.state}
+          label="State"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select defaultValue={request.state}>
             {states.map(state => (
               <Option value={state}>{state}</Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item label="Zip">
+        <Form.Item
+          name="zipCode"
+          initialValue={request.zipCode}
+          label="Zip"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
           <Input defaultValue={request.zipCode} />
         </Form.Item>
       </Form>
