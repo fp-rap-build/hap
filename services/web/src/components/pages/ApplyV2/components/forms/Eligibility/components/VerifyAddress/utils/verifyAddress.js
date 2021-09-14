d15c6da219c34c955d40eb7a04f22c5e0372f0a1
iesous-kurios <j.wylie.81@gmail.com>
2021-09-13 19:36:45 -0700

    got it working locally, pushing to dev for testing

--- services/web/src/components/pages/ApplyV2/components/forms/Eligibility/components/VerifyAddress/utils/verifyAddress.js
+++ services/web/src/components/pages/ApplyV2/components/forms/Eligibility/components/VerifyAddress/utils/verifyAddress.js
@@ -3,20 +3,20 @@ const SmartyStreetsCore = SmartyStreetsSDK.core;
 const Lookup = SmartyStreetsSDK.usStreet.Lookup;
 
 // for Server-to-server requests, use this code:
-// let authId = process.env.SMARTY_AUTH_ID;
-// let authToken = process.env.SMARTY_AUTH_TOKEN;
-// const credentials = new SmartyStreetsCore.StaticCredentials(authId, authToken);
+let authId = process.env.SMARTY_AUTH_ID;
+let authToken = process.env.SMARTY_AUTH_TOKEN;
+const credentials = new SmartyStreetsCore.StaticCredentials(authId, authToken);
 
 // for client-side requests (browser/mobile), use this code:
-let key = process.env.REACT_APP_SMARTY_KEY;
+//  let key = process.env.REACT_APP_SMARTY_KEY;
 
-const credentials = new SmartyStreetsCore.SharedCredentials(key);
+//const credentials = new SmartyStreetsCore.SharedCredentials(key);
 
 // The appropriate license values to be used for your subscriptions
 // can be found on the Subscription page of the account dashboard.
 // https://www.smartystreets.com/docs/cloud/licensing
 
-export default function verifyAddress(state, city, address, zipCode) {
+export default function verifyAddress(address, city, state, zipCode) {
   let clientBuilder = new SmartyStreetsCore.ClientBuilder(
     credentials
   ).withBaseUrl('https://us-street.api.smartystreets.com/street-address');
