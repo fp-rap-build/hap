0a69aa15acb1b7e46169f39f5d1b68b9d8df0fc1
Isaiah <greatpup1940@gmail.com>
2021-09-17 19:39:59 -0400

    Got rid of unnecessary comments

--- services/web/src/components/pages/ApplyV2/components/forms/Eligibility/components/VerifyAddress/index.tsx
+++ services/web/src/components/pages/ApplyV2/components/forms/Eligibility/components/VerifyAddress/index.tsx
@@ -26,12 +26,6 @@ const SmartyStreetsSDK = require('smartystreets-javascript-sdk');
 const SmartyStreetsCore = SmartyStreetsSDK.core;
 const Lookup = SmartyStreetsSDK.usAutocompletePro.Lookup;
 
-// for Server-to-server requests, use this code:
-// let authId = process.env.SMARTY_AUTH_ID;
-// let authToken = process.env.SMARTY_AUTH_TOKEN;
-// const credentials = new SmartyStreetsCore.StaticCredentials(authId, authToken);
-
-// for client-side requests (browser/mobile), use this code:
 let key = process.env.REACT_APP_SMARTY_KEY;
 
 const credentials = new SmartyStreetsCore.SharedCredentials(key);
