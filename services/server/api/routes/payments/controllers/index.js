32abd2201d865ef6ffd196f43c3897c856a8f352
Isaiah <greatpup1940@gmail.com>
2021-12-06 16:18:13 -0500

    Created endpoints for approving and denying payments

--- services/server/api/routes/payments/controllers/index.js
+++ services/server/api/routes/payments/controllers/index.js
@@ -1,5 +1,10 @@
 const Payments = require('../model');
 
+const Requests = require('../../requests/requestsModel');
+const {
+  sendConfirmationOfApproval,
+} = require('../../../utils/sendGrid/messages');
+
 exports.findForTable = async (req, res, next) => {
   try {
     const tableData = await Payments.findForTable();
@@ -14,10 +19,26 @@ exports.findForTable = async (req, res, next) => {
 exports.updatePayment = async (req, res, next) => {
   const { id } = req.params;
 
-  const { amount, monthsBack, totalArrears, monthsForward, amountBack, amountForward, processed } = req.body;
+  const {
+    amount,
+    monthsBack,
+    totalArrears,
+    monthsForward,
+    amountBack,
+    amountForward,
+    processed,
+  } = req.body;
 
   try {
-    const updatedPayment = await Payments.findByIdAndUpdate(id, { amount, monthsBack, monthsForward, amountBack, totalArrears, amountForward, processed });
+    const updatedPayment = await Payments.findByIdAndUpdate(id, {
+      amount,
+      monthsBack,
+      monthsForward,
+      amountBack,
+      totalArrears,
+      amountForward,
+      processed,
+    });
     res.status(200).json({ payment: updatedPayment });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error1' });
@@ -33,5 +54,47 @@ exports.deletePayment = async (req, res, next) => {
     res.status(204).json({ message: 'Payment has been deleted' });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+exports.approvePayment = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const payment = await Payments.findById(id);
+
+    if (payment.status === 'approved') {
+      return res
+        .status(400)
+        .json({ message: 'Payment has already been approved' });
+    }
+
+    const request = await Requests.findById(payment.id);
+
+    const approvedPayment = await Payments.findByIdAndUpdate(id, {
+      status: 'approved',
+    });
+
+    sendConfirmationOfApproval(request);
+
+    res.status(200).json({
+      payment: approvedPayment[0],
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Unable to approve payment' });
+  }
+};
+
+exports.denyPayment = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deniedPayment = await Payments.findByIdAndUpdate(id, {
+      status: 'denied',
+    });
+
+    res.status(200).json({ payment: deniedPayment[0] });
+  } catch (error) {
+    res.status(500).json({ message: 'Unable to deny payment' });
   }
 };
--- services/server/api/routes/payments/index.js
+++ services/server/api/routes/payments/index.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const restrictTo = require('../../middleware/restrictTo');
 
 // Controllers
-const { findForTable, updatePayment, deletePayment } = require('./controllers');
+const {
+  findForTable,
+  updatePayment,
+  deletePayment,
+  approvePayment,
+  denyPayment,
+} = require('./controllers');
 
 // Global middleware
 router.use(authRequired);
@@ -12,6 +18,10 @@ router.use(authRequired);
 router.use(restrictTo('admin', 'orgAdmin', 'programManager'));
 
 router.route('/:id').put(updatePayment).delete(deletePayment);
+
+router.route('/:id/approve').post(approvePayment);
+
+router.route('/:id/deny').post(denyPayment);
 
 // Routes
 router.route('/table').get(findForTable);
