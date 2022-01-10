48c311f302cd81f585aa31a3612cc957a0847df0
Isaiah <greatpup1940@gmail.com>
2022-01-10 18:49:46 -0500

    Created function for processing UPAF documents

--- services/web/src/components/pages/Admin/components/components/AttachmentViewer/components/UpafUpload.js
+++ services/web/src/components/pages/Admin/components/components/AttachmentViewer/components/UpafUpload.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 import { Button } from 'antd';
 
+import { processLLDoc } from '../../../../../../../utils/pandaDocUtils';
+
 export default function UpafUpload() {
+  const createPandaDoc = async docPayload => {
+    setLoadingDoc(true);
+    try {
+      const res = await processLLDoc(docPayload);
+      setDocumentInfo(res);
+    } catch (error) {
+      console.log(error);
+      alert('Error creating document');
+    } finally {
+      setLoadingDoc(false);
+    }
+  };
+
+  const handleClick = () => {
+    createPandaDoc(w9DocumentPayload);
+    setSelectedCategory('landlordW9');
+  };
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <p>Create</p>
